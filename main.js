1d1b099c2fa2f7eeedf8a0abbd21135cc29421f6
laurapedrosaflores-debug <laura.pedrosa.flores@escola.pr.gov.br>
2025-10-23 10:27:16 -0300

    k

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const atual = document.querySelector('.ativo')
 const proximoPasso='passo-'+this.getAttribute('data-proximo')
 
 atual.classList.remove('ativo')
-document.getElementById(proximoPasso).classList.add('.ativo')
+document.getElementById(proximoPasso).classList.add('ativo')
        })
 
        
