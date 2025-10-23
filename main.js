a8be6b1890cae0dcce8b8e61a42db6328851dbde
laurapedrosaflores-debug <laura.pedrosa.flores@escola.pr.gov.br>
2025-10-23 10:15:17 -0300

    l

--- main.js
+++ main.js
@@ -2,5 +2,9 @@ const avanca = document.querySelectorAll('.btn-proximo')
 console.log(avanca)
 
 avanca.forEach(button => {
+       button.addEventListener('click',function(){
+const atual = document.querySelector('.ativo')
+       })
 
+       
 })
